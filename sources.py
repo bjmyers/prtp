5562ecbdc8152477919794d827772202dbf5dd87
bjmyers <43039854+bjmyers@users.noreply.github.com>
2019-02-11 11:37:42 -0500

    Made array generation more consistent

--- sources.py
+++ sources.py
@@ -34,13 +34,13 @@ def pointsource(ang, num):
     l = rho*np.cos(theta)
     m = rho*np.sin(theta)
     n = np.sqrt(1.-l**2-m**2)
-    x = np.repeat(0., num)
-    y = np.repeat(0., num)
-    z = np.repeat(0., num)
-    ux = np.repeat(0., num)
-    uy = np.repeat(0., num)
-    uz = np.repeat(0., num)
-    opd = np.repeat(0., num)
+    x = np.zeros(num)
+    y = np.zeros(num)
+    z = np.zeros(num)
+    ux = np.zeros(num)
+    uy = np.zeros(num)
+    uz = np.zeros(num)
+    opd = np.zeros(num)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
 
@@ -68,14 +68,14 @@ def circularbeam(rad, num):
     theta = np.random.rand(num)*2*np.pi
     x = rho*np.cos(theta)
     y = rho*np.sin(theta)
-    z = np.repeat(0., num)
-    l = np.repeat(0., num)
-    m = np.repeat(0., num)
-    n = np.repeat(1., num)
-    ux = np.copy(l)
-    uy = np.copy(l)
-    uz = np.copy(l)
-    opd = np.copy(l)
+    z = np.zeros(num)
+    l = np.zeros(num)
+    m = np.zeros(num)
+    n = np.ones(num)
+    ux = np.zeros(num)
+    uy = np.zeros(num)
+    uz = np.zeros(num)
+    opd = np.zeros(num)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
 
@@ -107,14 +107,14 @@ def annulus(rin, rout, num, zhat=-1.):
     theta = np.random.rand(num)*2*np.pi
     x = rho*np.cos(theta)
     y = rho*np.sin(theta)
-    z = np.repeat(0., num)
-    l = np.repeat(0., num)
-    m = np.repeat(0., num)
-    n = np.repeat(zhat, num)
-    ux = np.copy(l)
-    uy = np.copy(l)
-    uz = np.copy(l)
-    opd = np.copy(l)
+    z = np.zeros(num)
+    l = np.zeros(num)
+    m = np.zeros(num)
+    n = np.ones(num) * zhat
+    ux = np.zeros(num)
+    uy = np.zeros(num)
+    uz = np.zeros(num)
+    opd = np.zeros(num)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
 
@@ -150,14 +150,14 @@ def subannulus(rin, rout, dphi, num, zhat=1.):
     theta = np.random.rand(num)*dphi - dphi/2.
     x = rho*np.cos(theta)
     y = rho*np.sin(theta)
-    z = np.repeat(0., num)
-    l = np.repeat(0., num)
-    m = np.repeat(0., num)
-    n = np.repeat(zhat, num)
-    ux = np.copy(l)
-    uy = np.copy(l)
-    uz = np.copy(l)
-    opd = np.copy(l)
+    z = np.zeros(num)
+    l = np.zeros(num)
+    m = np.zeros(num)
+    n = np.ones(num) * zhat
+    ux = np.zeros(num)
+    uy = np.zeros(num)
+    uz = np.zeros(num)
+    opd = np.zeros(num)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
 
@@ -186,15 +186,15 @@ def xslit(xin, xout, num, zhat=-1.):
 
     '''
     x = np.linspace(xin, xout, num)
-    y = np.repeat(0., num)
-    z = np.repeat(0., num)
-    l = np.repeat(0., num)
-    m = np.repeat(0., num)
-    n = np.repeat(zhat, num)
-    ux = np.copy(l)
-    uy = np.copy(l)
-    uz = np.copy(l)
-    opd = np.copy(l)
+    y = np.zeros(num)
+    z = np.zeros(num)
+    l = np.zeros(num)
+    m = np.zeros(num)
+    n = np.ones(num) * zhat
+    ux = np.zeros(num)
+    uy = np.zeros(num)
+    uz = np.zeros(num)
+    opd = np.zeros(num)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
 
@@ -229,12 +229,12 @@ def rectArray(xsize, ysize, num):
     x = x.flatten()
     y = y.flatten()
     z = np.copy(opd)
-    l = np.repeat(0., num**2)
-    m = np.repeat(0., num**2)
-    n = np.repeat(1., num**2)
-    ux = np.copy(l)
-    uy = np.copy(l)
-    uz = np.copy(l)
+    l = np.zeros(num**2)
+    m = np.zeros(num**2)
+    n = np.ones(num**2)
+    ux = np.zeros(num**2)
+    uy = np.zeros(num**2)
+    uz = np.zeros(num**2)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
 
@@ -274,16 +274,16 @@ def convergingbeam(zset, rin, rout, tmin, tmax, num, lscat):
     theta = tmin + np.random.rand(num)*(tmax-tmin)
     x = rho*np.cos(theta)
     y = rho*np.sin(theta)
-    z = np.repeat(zset, num)
+    z = np.ones(num) * zset
     lscat = lscat * np.tan((np.random.rand(num) - .5)*np.pi)
     lscat = lscat/60**2 * np.pi/180.
     n = -np.cos(np.arctan(rho/zset)+lscat)
     l = -np.sqrt(1-n**2)*np.cos(theta)
     m = -np.sqrt(1-n**2)*np.sin(theta)
-    ux = np.repeat(0., num)
-    uy = np.repeat(0., num)
-    uz = np.repeat(0., num)
-    opd = np.repeat(0., num)
+    ux = np.zeros(num)
+    uy = np.zeros(num)
+    uz = np.zeros(num)
+    opd = np.zeros(num)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
 
@@ -323,16 +323,16 @@ def convergingbeam2(zset, xmin, xmax, ymin, ymax, num, lscat):
     y = ymin + np.random.rand(num)*(ymax-ymin)
     rho = np.sqrt(x**2+y**2)
     theta = np.arctan2(y, x)
-    z = np.repeat(zset, num)
+    z = np.ones(num) * zset
     lscat = lscat * np.tan((np.random.rand(num) - .5)*np.pi)
     lscat = lscat/60**2 * np.pi/180.
     n = -np.cos(np.arctan(rho/zset)+lscat)
     l = -np.sqrt(1-n**2)*np.cos(theta)
     m = -np.sqrt(1-n**2)*np.sin(theta)
-    ux = np.repeat(0., num)
-    uy = np.repeat(0., num)
-    uz = np.repeat(0., num)
-    opd = np.repeat(0., num)
+    ux = np.zeros(num)
+    uy = np.zeros(num)
+    uz = np.zeros(num)
+    opd = np.zeros(num)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
 
@@ -359,13 +359,13 @@ def rectbeam(xhalfwidth, yhalfwidth, num):
     '''
     x = (np.random.rand(num)-.5)*2*xhalfwidth
     y = (np.random.rand(num)-.5)*2*yhalfwidth
-    z = np.repeat(0., num)
-    n = np.repeat(1., num)
-    l = np.copy(z)
-    m = np.copy(z)
-    ux = np.repeat(0., num)
-    uy = np.repeat(0., num)
-    uz = np.repeat(0., num)
-    opd = np.repeat(0., num)
+    z = np.zeros(num)
+    n = np.ones(num)
+    l = np.zeros(num)
+    m = np.zeros(num)
+    ux = np.zeros(num)
+    uy = np.zeros(num)
+    uz = np.zeros(num)
+    opd = np.zeros(num)
 
     return [opd, x, y, z, l, m, n, ux, uy, uz]
