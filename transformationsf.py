45f76f66a717a5525fa8d9f4a113a77da9b520bb
bjmyers <43039854+bjmyers@users.noreply.github.com>
2019-03-20 12:34:08 -0400

    Made transformationsf compatible with Rays objects

--- transformationsf.py
+++ transformationsf.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numba import jit
 #'UniTuple(float64[:], 2)(float64[:],float64[:],float64[:],int32,int32)'
-@jit(cache=True,nopython=True)
+#@jit(cache=True,nopython=True)
 def rotatevector(x,y,z,theta,axis):
     #This function rotates a vector in a right handed fashion
     #axis is 1,2,3 for x,y,z axis rotation
@@ -35,31 +35,23 @@ def rotateaxis(x,y,z,ux,uy,uz,theta):
     tempx = (c+ux**2*(1-c))*x + (ux*uy*(1-c)-uz*s)*y + (ux*uz*(1-c)+uy*s)*z
     tempy = (uy*ux*(1-c)+uz*s)*x + (c+uy**2*(1-c))*y + (uy*uz*(1-c)-ux*s)*z
     z = (uz*ux*(1-c)-uy*s)*x + (uz*uy*(1-c)+ux*s)*y + (c+uz**2*(1-c))*z
-    return tempx,tempy,z
+    return tempx,tempy,z,ux,uy,uz
 
 #'float[:,:](float64[:,:])'
-@jit(cache=True,nopython=True)  
+#@jit(cache=True,nopython=True)  
 def reflect(rays):
-    # Temp list so inputs are not modified
-    raycopy = []
-    for lst in rays:
-        raycopy.append(lst.copy())
-    opd,x,y,z,l,m,n,ux,uy,uz = raycopy
+    x,y,z,l,m,n,ux,uy,uz =         rays.x,rays.y,rays.z,rays.l,rays.m,rays.n,rays.ux,rays.uy,rays.uz
 
     dot = ux*l + uy*m + uz*n
     l -= 2*dot*ux
     m -= 2*dot*uy
     n -= 2*dot*uz
     
-    return [opd, x, y, z, l, m, n, ux, uy, uz]
+    return x, y, z, l, m, n, ux, uy, uz
     
-    'UniTuple(float64[:], 2)(float64[:],float64[:],float64[:],int32,int32)'
+#'UniTuple(float64[:], 2)(float64[:],float64[:],float64[:],int32,int32)'
 def refract(rays,n1,n2):
-    # Temp list so inputs are not modified
-    raycopy = []
-    for lst in rays:
-        raycopy.append(lst.copy())
-    opd,x,y,z,l,m,n,ux,uy,uz = raycopy
+    x,y,z,l,m,n,ux,uy,uz =         rays.x,rays.y,rays.z,rays.l,rays.m,rays.n,rays.ux,rays.uy,rays.uz
     
     dot = ux*l + uy*m + uz*n
     negativedot = (dot<0)
@@ -75,26 +67,22 @@ def refract(rays,n1,n2):
     cy = l*uz-ux*n
     cz = ux*m-l*uy
     
-    l,m,n = rotateaxis(l, m, n, cx, cy, cz, t2-t1)
+    l,m,n,cx,cy,cz = rotateaxis(l, m, n, cx, cy, cz, t2-t1)
     alpha = np.sqrt(l**2 + m**2 + n**2)
     l = np.where(unfinishedrays,l/alpha,l)
     m = np.where(unfinishedrays,m/alpha,m)
     n = np.where(unfinishedrays,n/alpha,n)
     
-    return [opd, x, y, z, l, m, n, ux, uy, uz]
+    return x, y, z, l, m, n, ux, uy, uz
     
-@jit(cache=True,nopython=True)
+#@jit(cache=True,nopython=True)
 def transform(rays,tx,ty,tz,rx,ry,rz):
     # Coordinate system transform, translations are done first, then rotations in XYZ order
     # Rotations act to rotate a surface via the right hand rule
     # tx,ty,tz are linear translations
     # rx,ry,rz are angular translations
     
-    # Temp list so inputs are not modified
-    raycopy = []
-    for lst in rays:
-        raycopy.append(lst.copy())
-    opd,x,y,z,l,m,n,ux,uy,uz = raycopy
+    x,y,z,l,m,n,ux,uy,uz =         rays.x,rays.y,rays.z,rays.l,rays.m,rays.n,rays.ux,rays.uy,rays.uz
 
     tx *= -1
     ty *= -1
@@ -119,48 +107,40 @@ def transform(rays,tx,ty,tz,rx,ry,rz):
     l,m,n = rotatevector(l,m,n,rz,3)
     ux,uy,uz = rotatevector(ux,uy,uz,rz,3)
     
-    return [opd, x, y, z, l, m, n, ux, uy, uz]
+    return x, y, z, l, m, n, ux, uy, uz
     
     
 def itransform(rays,tx,ty,tz,rx,ry,rz):
     # This function inverts the transform function with the same arguments
     # transform(rays,0,10,0,2,0,0) will be undone by itransform(rays,0,10,0,2,0,0)
     
-    # Temp list so inputs are not modified
-    raycopy = []
-    for lst in rays:
-        raycopy.append(lst.copy())
-    opd,x,y,z,l,m,n,ux,uy,uz = raycopy
+    x,y,z,l,m,n,ux,uy,uz =         rays.x,rays.y,rays.z,rays.l,rays.m,rays.n,rays.ux,rays.uy,rays.uz
     
-    x,y,z = rotatevector(x,y,z,-rz,3)
-    l,m,n = rotatevector(l,m,n,-rz,3)
-    ux,uy,uz = rotatevector(ux,uy,uz,-rz,3)
+    x,y,z = rotatevector(x,y,z,rz,3)
+    l,m,n = rotatevector(l,m,n,rz,3)
+    ux,uy,uz = rotatevector(ux,uy,uz,rz,3)
     
-    x,y,z = rotatevector(x,y,z,-ry,2)
-    l,m,n = rotatevector(l,m,n,-ry,2)
-    ux,uy,uz = rotatevector(ux,uy,uz,-ry,2)
+    x,y,z = rotatevector(x,y,z,ry,2)
+    l,m,n = rotatevector(l,m,n,ry,2)
+    ux,uy,uz = rotatevector(ux,uy,uz,ry,2)
     
-    x,y,z = rotatevector(x,y,z,-rx,1)
-    l,m,n = rotatevector(l,m,n,-rx,1)
-    ux,uy,uz = rotatevector(ux,uy,uz,-rx,1)
+    x,y,z = rotatevector(x,y,z,rx,1)
+    l,m,n = rotatevector(l,m,n,rx,1)
+    ux,uy,uz = rotatevector(ux,uy,uz,rx,1)
     
-    x -= tx
-    y -= ty
-    z -= tz
+    x += tx
+    y += ty
+    z += tz
     
-    return [opd, x, y, z, l, m, n, ux, uy, uz]
+    return x, y, z, l, m, n, ux, uy, uz
 
 
-def radgrat(rays,wave,dpermm,order,eliminate="nan"):
+def radgrat(rays,wave,dpermm,order):
     # Radially grooved grating diffraction
     # Assumes grating in x y plane, with grooves converging at 
     # hubdist in positive y direction
     
-    # Temp list so inputs are not modified
-    raycopy = []
-    for lst in rays:
-        raycopy.append(lst.copy())
-    opd,x,y,z,l,m,n,ux,uy,uz = raycopy
+    x,y,z,l,m,n,ux,uy,uz =         rays.x,rays.y,rays.z,rays.l,rays.m,rays.n,rays.ux,rays.uy,rays.uz
     
     sn = n / np.abs(n)
     d = dpermm * np.sqrt(y**2 + x**2)
@@ -172,33 +152,20 @@ def radgrat(rays,wave,dpermm,order,eliminate="nan"):
         n = np.sqrt(1.0 - l**2 - m**2)
     
     # Check for Evanescence
-    removelist = ((l**2 + m**2) > 1)
+    with np.errstate(invalid='ignore'):
+        removelist = ((l**2 + m**2) > 1)
     
-    if (eliminate.lower() == 'nan'):
-        x[removelist] = np.nan
-        y[removelist] = np.nan
-        z[removelist] = np.nan
-        l[removelist] = np.nan
-        m[removelist] = np.nan
-        n[removelist] = np.nan
-        ux[removelist] = np.nan
-        uy[removelist] = np.nan
-        uz[removelist] = np.nan
-        opd[removelist] = np.nan
+    x[removelist] = np.nan
+    y[removelist] = np.nan
+    z[removelist] = np.nan
+    l[removelist] = np.nan
+    m[removelist] = np.nan
+    n[removelist] = np.nan
+    ux[removelist] = np.nan
+    uy[removelist] = np.nan
+    uz[removelist] = np.nan
     
-    elif (eliminate.lower() == 'remove'):
-        x = x[np.logical_not(removelist)]
-        y = y[np.logical_not(removelist)]
-        z = z[np.logical_not(removelist)]
-        l = l[np.logical_not(removelist)]
-        m = m[np.logical_not(removelist)]
-        n = n[np.logical_not(removelist)]
-        ux = ux[np.logical_not(removelist)]
-        uy = uy[np.logical_not(removelist)]
-        uz = uz[np.logical_not(removelist)]
-        opd = opd[np.logical_not(removelist)]
-    
-    return [opd, x, y, z, l, m, n, ux, uy, uz]
+    return x, y, z, l, m, n, ux, uy, uz
 
 
 def radgratW(rays,wave,dpermm,order, eliminate="nan"):
@@ -206,11 +173,7 @@ def radgratW(rays,wave,dpermm,order, eliminate="nan"):
     # Assumes grating in x y plane, with grooves converging at 
     # hubdist in positive y direction
     
-    # Temp list so inputs are not modified
-    raycopy = []
-    for lst in rays:
-        raycopy.append(lst.copy())
-    opd,x,y,z,l,m,n,ux,uy,uz = raycopy
+    x,y,z,l,m,n,ux,uy,uz =         rays.x,rays.y,rays.z,rays.l,rays.m,rays.n,rays.ux,rays.uy,rays.uz
     
     d = dpermm * np.sqrt(y**2 + x**2)
     sn = y/np.abs(y)
@@ -221,33 +184,20 @@ def radgratW(rays,wave,dpermm,order, eliminate="nan"):
     n = np.sqrt(1.0 - l**2 - m**2)
     
     # Check for Evanescence
-    removelist = ((l**2 + m**2) > 1)
+    with np.errstate(invalid='ignore'):
+        removelist = ((l**2 + m**2) > 1)
+
+    x[removelist] = np.nan
+    y[removelist] = np.nan
+    z[removelist] = np.nan
+    l[removelist] = np.nan
+    m[removelist] = np.nan
+    n[removelist] = np.nan
+    ux[removelist] = np.nan
+    uy[removelist] = np.nan
+    uz[removelist] = np.nan
     
-    if (eliminate.lower() == 'nan'):
-        x[removelist] = np.nan
-        y[removelist] = np.nan
-        z[removelist] = np.nan
-        l[removelist] = np.nan
-        m[removelist] = np.nan
-        n[removelist] = np.nan
-        ux[removelist] = np.nan
-        uy[removelist] = np.nan
-        uz[removelist] = np.nan
-        opd[removelist] = np.nan
-    
-    elif (eliminate.lower() == 'remove'):
-        x = x[np.logical_not(removelist)]
-        y = y[np.logical_not(removelist)]
-        z = z[np.logical_not(removelist)]
-        l = l[np.logical_not(removelist)]
-        m = m[np.logical_not(removelist)]
-        n = n[np.logical_not(removelist)]
-        ux = ux[np.logical_not(removelist)]
-        uy = uy[np.logical_not(removelist)]
-        uz = uz[np.logical_not(removelist)]
-        opd = opd[np.logical_not(removelist)]
-    
-    return [opd, x, y, z, l, m, n, ux, uy, uz]
+    return x, y, z, l, m, n, ux, uy, uz
     
     
 def grat(rays,d,order,wave, eliminate="nan"):
@@ -255,45 +205,27 @@ def grat(rays,d,order,wave, eliminate="nan"):
     # Wavelength wave
     # Groove direction assumed in y direction
     
-    # Temp list so inputs are not modified
-    raycopy = []
-    for lst in rays:
-        raycopy.append(lst.copy())
-    opd,x,y,z,l,m,n,ux,uy,uz = raycopy
+    x,y,z,l,m,n,ux,uy,uz =         rays.x,rays.y,rays.z,rays.l,rays.m,rays.n,rays.ux,rays.uy,rays.uz
     
     l -= order*wave/d
     with np.errstate(invalid='ignore'):
         n = np.sqrt(1.0 - l**2 - m**2)
     
     # Check for Evanescence
-    removelist = ((l**2 + m**2) > 1)
-    
-    if (eliminate.lower() == 'nan'):
-        x[removelist] = np.nan
-        y[removelist] = np.nan
-        z[removelist] = np.nan
-        l[removelist] = np.nan
-        m[removelist] = np.nan
-        n[removelist] = np.nan
-        ux[removelist] = np.nan
-        uy[removelist] = np.nan
-        uz[removelist] = np.nan
-        opd[removelist] = np.nan
-    
-    elif (eliminate.lower() == 'remove'):
-        x = x[np.logical_not(removelist)]
-        y = y[np.logical_not(removelist)]
-        z = z[np.logical_not(removelist)]
-        l = l[np.logical_not(removelist)]
-        m = m[np.logical_not(removelist)]
-        n = n[np.logical_not(removelist)]
-        ux = ux[np.logical_not(removelist)]
-        uy = uy[np.logical_not(removelist)]
-        uz = uz[np.logical_not(removelist)]
-        opd = opd[np.logical_not(removelist)]
+    with np.errstate(invalid='ignore'):
+        removelist = ((l**2 + m**2) > 1)
+
+    x[removelist] = np.nan
+    y[removelist] = np.nan
+    z[removelist] = np.nan
+    l[removelist] = np.nan
+    m[removelist] = np.nan
+    n[removelist] = np.nan
+    ux[removelist] = np.nan
+    uy[removelist] = np.nan
+    uz[removelist] = np.nan
         
-    return [opd, x, y, z, l, m, n, ux, uy, uz]
-    
+    return x, y, z, l, m, n, ux, uy, uz 
    
     
     
