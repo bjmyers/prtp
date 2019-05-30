4e21131d832a429b587dd0bf072f593666f8a26b
bjmyers <43039854+bjmyers@users.noreply.github.com>
2019-05-30 15:44:16 -0400

    Updated basicintrument for Sources objects
    prtpbasicinstrument.py now uses Sources Objects correctly. Also improved comments and made code style more consistent with what was intended

--- Examples/prtpbasicinstrument.py
+++ Examples/prtpbasicinstrument.py
@@ -6,6 +6,7 @@ from prtp.WolterTypeOne import WolterTypeOne
 from prtp.Grating import Grating
 from prtp.Instrument import Instrument
 from prtp.Modification import Modification
+from prtp.Sources.Subannulus import Subannulus
 
 # Define Wolter-I parameters.
 r0 = 165.  # Radius at the intersection plane of primary/secondary.
@@ -22,29 +23,36 @@ rp_front = prtp.conicsolve.primrad(z0 + mirror_sep/2 + mirror_len, r0, z0)
 rp_back = prtp.conicsolve.primrad(z0 + mirror_sep/2, r0, z0)
 
 # Define initial rays in subannulus.
-rays = Rays.subannulus(rp_back, rp_front, np.radians(30.), 1000)
+source = Subannulus(1000,rp_back, rp_front, np.radians(30.),wave=0.83401,order=-2)
 
-# Rotate 90 degrees so that diffraction occurs in x-axis.
-rays.transform(0, 0, 0, 0, 0, np.radians(90.))
+# Define Rotation Modification to change the Rays so that diffraction
+# occurs in the x-axis
+def rot(rays,cw):
+    rays.transform(0, 0, 0, 0, 0, np.radians(90.))
+rotation = Modification(rot)
 
-w = WolterTypeOne(r0=r0,z0=z0)
-g = Grating(0.,151.86466758,3247.56521956,
+# Define Wolter Optic
+wolter = WolterTypeOne(r0=r0,z0=z0)
+
+# Define Grating (values from old code)
+grat = Grating(0.,151.86466758,3247.56521956,
             0.,0.99978888,-0.0205472,
             0.01518378,0.02054483,0.99967363,
             l=100,w=100,d=d,radial=True,fdist=3250)
 
 def func(rays,cw):
     rays.beckmann_scatter(0,0,1.48e-5)
-    
-m = Modification(func)
+scatter = Modification(func)
 
-i = Instrument(rays,waves=0.83401,orders=-2,weighting=False)
-i.addComponent(w)
-i.addComponent(m)
-i.addComponent(g)
+i = Instrument(source)
+i.addComponent(rotation)
+i.addComponent(wolter)
+i.addComponent(scatter)
+i.addComponent(grat)
 i.simulate()
 i.displayEfficiency()
 
+rays = i.getRays()
 
 rays.focusX()
 rays.scatter3d()
