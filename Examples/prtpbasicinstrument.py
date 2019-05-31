1c077c99421af2cf6ec0c858c53b60de6f140235
bjmyers <43039854+bjmyers@users.noreply.github.com>
2019-05-31 15:46:34 -0400

    Updated Example with astropy units

--- Examples/prtpbasicinstrument.py
+++ Examples/prtpbasicinstrument.py
@@ -2,28 +2,29 @@ import numpy as np
 import prtp
 import prtp.conicsolve
 from prtp.Rays import Rays
-from prtp.WolterTypeOne import WolterTypeOne
+from prtp.WolterOptic import WolterTypeOne
 from prtp.Grating import Grating
 from prtp.Instrument import Instrument
 from prtp.Modification import Modification
-from prtp.Sources.Subannulus import Subannulus
+from prtp.Sources import Subannulus
+import astropy.units as u
 
 # Define Wolter-I parameters.
-r0 = 165.  # Radius at the intersection plane of primary/secondary.
-z0 = 3500. # 
-mirror_len = 100.
-mirror_sep = 5.
+r0 = (165. * u.mm).to('parsec')  # Radius at the intersection plane of primary/secondary.
+z0 = 3500. * u.mm 
+mirror_len = 100. * u.mm
+mirror_sep = 5. * u.mm
 
-d = 160.  # Groove period @ 3300 mm [nm]
-L = 3250.  # Center of grating [mm]
-d *= L  / 3300  # Find groove period at center of grating.
+d = 160. * u.nm  # Groove period @ 3300 mm [nm]
+L = 3250. * u.mm # Center of grating [mm]
+d *= L  / (3300 * u.mm)  # Find groove period at center of grating.
 
 # Define inner & outer radii to create rays.
 rp_front = prtp.conicsolve.primrad(z0 + mirror_sep/2 + mirror_len, r0, z0)
 rp_back = prtp.conicsolve.primrad(z0 + mirror_sep/2, r0, z0)
 
 # Define initial rays in subannulus.
-source = Subannulus(1000,rp_back, rp_front, np.radians(30.),wave=0.83401,order=-2)
+source = Subannulus(1000,rp_back, rp_front, np.radians(30.)*u.rad,wave=0.83401*u.nm,order=0)
 
 # Define Rotation Modification to change the Rays so that diffraction
 # occurs in the x-axis
@@ -35,10 +36,10 @@ rotation = Modification(rot)
 wolter = WolterTypeOne(r0=r0,z0=z0)
 
 # Define Grating (values from old code)
-grat = Grating(0.,151.86466758,3247.56521956,
+grat = Grating(0.*u.mm,151.86466758*u.mm,3247.56521956*u.mm,
             0.,0.99978888,-0.0205472,
             0.01518378,0.02054483,0.99967363,
-            l=100,w=100,d=d,radial=True,fdist=3250)
+            l=100*u.mm,w=100*u.mm,d=d,radial=True,fdist=3250*u.mm)
 
 def func(rays,cw):
     rays.beckmann_scatter(0,0,1.48e-5)
