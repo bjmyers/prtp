d4ce47d22070aebcb1f4920508da0768b05d11be
bjmyers <43039854+bjmyers@users.noreply.github.com>
2019-06-04 13:12:24 -0400

    Added WolterModule
    The WolterModule is a series of nested Wolter Type I objects. Photons are traced to the mirrors in the same way they are traced to Grating Stacks.

--- WolterOptic.py
+++ WolterOptic.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 from prtp.Rays import Rays
 import prtp.specialFunctions as func
 import prtp.transformationsf as trans
+from prtp.Combination import Combination
 import astropy.units as u
+
 
 class WolterOptic:
     
@@ -408,11 +410,17 @@ class WolterTypeOne(WolterOptic):
 
     ## Tracing Rays to the Optic:
     
-    def tracefunction(self,rays,autoreflect):
+    def tracefunction(self,rays,autoreflect,eliminate='remove'):
         '''
         Function tracefunction:
         Determines what happens to the rays once they has been transformed, 
             should only be called by the WolterOptic superclass's trace function
+        
+        Notes:
+        -The eliminate parameter has two options, "remove" and "nan". For almost
+            all cases it will be "remove." But WolterModule objects require 
+            it to be set to "nan", in which eliminated rays are set to NaN,
+            rather than removed altogether from the Rays object
         '''
         rays.wolterprimary(self.r0.value,self.z0.value,self.psi)
         
@@ -420,16 +428,26 @@ class WolterTypeOne(WolterOptic):
         if self.axial_length is not None and self.mirror_sep is not None:
             tarray = np.logical_or((rays.z < (self.z0 + self.mirror_sep/2).value),
                     rays.z > (self.z0 + self.mirror_sep/2 + self.axial_length).value)
-            rays.remove(tarray)
+            if (eliminate.lower() == 'remove'):
+                rays.remove(tarray)
+            else:
+                rays.x[tarray] = np.nan
         
         rays.reflect()
         rays.woltersecondary(self.r0.value,self.z0.value,self.psi)
         
-        # Consider which photons missed the secondary mirror
-        if self.axial_length is not None and self.mirror_sep is not None:
-            tarray = np.logical_or((rays.z > (self.z0 - self.mirror_sep/2).value),
-                    rays.z < (self.z0 - self.mirror_sep/2 - self.axial_length).value)
-            rays.remove(tarray)
+        # When called with eliminate='nan', this block will cause runtime errors,
+        # we will ignore them using this next line
+        with np.errstate(invalid='ignore'):
+        
+            # Consider which photons missed the secondary mirror
+            if self.axial_length is not None and self.mirror_sep is not None:
+                tarray = np.logical_or((rays.z > (self.z0 - self.mirror_sep/2).value),
+                        rays.z < (self.z0 - self.mirror_sep/2 - self.axial_length).value)
+                if (eliminate.lower() == 'remove'):
+                    rays.remove(tarray)
+                else:
+                    rays.x[tarray] = np.nan
         
         if autoreflect:
             rays.reflect()
@@ -452,3 +470,137 @@ class WolterTypeOne(WolterOptic):
         l = rays.length(considerweights)
         self.tracehelper(rays,self.tracefunction,autoreflect,considerweights)
         return ("Missed Wolter Optic",l,rays.length(considerweights))
+
+
+class WolterModule(WolterOptic, Combination):
+    
+    @u.quantity_input(x=u.mm,y=u.mm,z=u.mm,r0=u.mm,z0=u.mm,axial_length=u.mm,mirror_sep=u.mm)
+    def __init__(self, x=0*u.mm,y=0*u.mm,z=0*u.mm,nx=0,ny=0,nz=1, 
+                r0=None, z0 = None, psi = 1,axial_length = None,mirror_sep = None):
+        '''
+        Function __init__:
+        Initializes a WolterModule Object. This object consists of a list of 
+            WolterTypeOne objects in the same way that a GratingStack consists
+            of a list of Grating Objects. The objects in a WolterModule Object
+            are nested together.
+        
+        Inputs:
+        x,y,z - The position of the focus of the optic, must be astropy units
+            of length
+        nx,ny,nz - Components of a normal vector pointing outwards from the focus 
+            of the optic
+        r0 - The radius of the optic when it converges with the WolterSecondary.
+            Must be an astropy unit of length
+        z0 - The position along the n-direction at which this optic converges .
+            Must be an astropy unit of length
+            with the WolterSecondary (the focus is at position 0)
+        psi - Some ratio of angles, I don't know, I think its always 1
+        axial_length - The length along the n-axis of the mirror, must be an 
+            astropy unit of length
+        mirror_sep - The separation between two mirrors, must be an astropy unit
+            of length
+        
+        
+        Notes:
+        - r0,z0,axial_length,and mirror_sep can be lists or numpy arrays of the 
+            same length with astropy units of length attached. If they are lists
+            of length n, then n WolterTypeOne objects will be added to the 
+            componentlist of this Module. Otherwise, Mirrors have to be added
+            individually.
+        - WolterModule also descends from WolterOptic, and as such has its own
+            parameters for x,y,z,nx,ny, and nz. Since every mirror in the Module
+            must have the same value for x,y,z,nx,ny, and nz, the Module's values
+            for these parameters is just the same as the values for each mirror.
+            These parameters are needed for the orientation algorithm 
+            tracehelper()
+        '''
+        Combination.__init__(self)
+        WolterOptic.__init__(self,x,y,z,nx,ny,nz)
+        if (r0 is not None or z0 is not None):
+            try:
+                lr = len(r0)
+                lz = len(z0)
+                la = len(axial_length)
+                lm = len(mirror_sep)
+                if (lr != lz or lr != la or lr != lm):
+                    raise ValueError('r0, z0, axial_length, and mirror_sep must be the same length!')
+                # Iterate through each list and add the Wolter Optic to 
+                # the component list
+                for i in range(lr):
+                    self.componentlist.append(WolterTypeOne(x,y,z,nx,ny,nz,r0[i],z0[i],psi,axial_length[i],mirror_sep[i]))
+            except:
+                # This will execute if r0 and z0 are single values
+                self.componentlist.append(WolterTypeOne(x,y,z,nx,ny,nz,r0,z0,psi,axial_length,mirror_sep))
+    
+    ## Tracing Rays to the Optic:
+    def tracefunction(self,rays,autoreflect=True):
+        '''
+        Function defaultTrace:
+        Traces the Rays through the Mirror Module in the order that the Mirrors
+            were added to the Module.
+        
+        Inputs:
+        rays - The rays you want to trace through the stack
+        autoreflect - Boolean saying if you want to automatically reflect the
+            rays.
+        
+        Outputs:
+        Nothing
+        '''
+        # Make a blank Rays object to store the Rays that make it
+        finalrays = Rays()
+        
+        # Keep track of the input rays for when we're finished with one Mirror
+        inputrays = rays.copy()
+        
+        # Iterate through each Mirror Object
+        for r in self.componentlist:
+            # Through each pass we need to ensure that the rays that make it are 
+            # placed into a final rays object
+            # All those that miss are passed to the next Mirror
+            r.tracefunction(rays,autoreflect,eliminate='nan')
+
+            # Find the Rays which missed the 
+            tarray = np.logical_not(np.isnan(rays.x))
+            hitrays = rays.split(tarray)
+            
+            # Make sure at least some rays have hit the grating
+            if (len(hitrays) == 0):
+                rays = inputrays.copy()
+                continue
+            
+            # Add the hitrays to our final tally
+            finalrays += hitrays
+            
+            # Take the rays that hit this grating out of the original Rays object
+            inputrays.remove(tarray)
+            
+            # Back remaining rays up to their original position
+            rays = inputrays.copy()
+            
+            # If there are no rays left, we can stop
+            if len(rays) == 0:
+                break
+        
+        # Make it so that the original rays now contain the output
+        rays.makecopy(finalrays)
+    
+    
+    def trace(self,rays,autoreflect=True,considerweights=False):
+        '''
+        Function trace:
+        Traces rays to the wolter module
+        
+        Inputs:
+        rays - The Rays object you want to trace to the optic
+        autoreflect - If True, photons will be reflected off of the optic's 
+            surface automatically
+        considerweights - Should be True if the photons are weighted
+        
+        Outputs:
+        eff - A tuple containing information about how many photons hit the
+            optic
+        '''
+        l = rays.length(considerweights)
+        self.tracehelper(rays,self.tracefunction,autoreflect,considerweights)
+        return ("Missed Wolter Module",l,rays.length(considerweights))
