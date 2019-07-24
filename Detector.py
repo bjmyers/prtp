2ffd079d49d6f83b279bd78a86f5c0e9c6023b4f
bjmyers <43039854+bjmyers@users.noreply.github.com>
2019-07-24 13:49:43 -0400

    Added Split Events
    The split() function allows for the simulation of photon's charge clouds to be split across several pixels

--- Detector.py
+++ Detector.py
@@ -4,14 +4,16 @@ from prtp.Rays import Rays
 from prtp.FlatComponent import FlatComponent
 import prtp.transformationsf as trans
 import astropy.units as u
+from scipy.integrate import quad
 
 class Detector(FlatComponent):
     
     ## Initialization Functions:
     
-    @u.quantity_input(x=u.mm,y=u.mm,z=u.mm,l=u.mm,w=u.mm)
+    @u.quantity_input(x=u.mm,y=u.mm,z=u.mm,l=u.mm,w=u.mm,fieldfree=u.um,depletion=u.um)
     def __init__(self,x=0*u.mm,y=0*u.mm,z=0*u.mm,nx=0,ny=0,nz=1,sx=0,sy=1,sz=0,
-        q=1.,l=1*u.mm,w=1*u.mm,xpix=10,ypix=10):
+        q=1.,l=1*u.mm,w=1*u.mm,xpix=10,ypix=10,fieldfree=15*u.um,depletion=3*u.um,
+        considersplits=False, steps=50):
         '''
         Initializes a Detector Object, requires the following arguments:
         
@@ -28,6 +30,18 @@ class Detector(FlatComponent):
             must be an astropy unit of length
         xpix - The number of pixels along the length of the detector
         ypix - The number of pixels along the width of the detector
+        fieldfree - The depth of the fieldfree region of the detector, default
+            units are in microns, must be in units of length.
+        depletion - The depth of the depletion region of the detector, is not
+            currently used. Must be in units of length, default units are 
+            microns.
+        considersplits - A boolean to determine if split events should be 
+            considered. Should be True if you want to simulate split events,
+            note that this will slow down the view() function significantly.
+        steps - If considersplits is True, the split events will be calculated
+            using this number of steps. Lower number of steps will be less 
+            accurate but will run faster. For small splits (taking up only a
+            few pixels), few steps is acceptable.
         
         Notes:
         -Unlike other FlatComponents, Detector needs a length and width, those 
@@ -39,6 +53,10 @@ class Detector(FlatComponent):
         self.w = w.to(u.mm)
         self.xpix = xpix
         self.ypix = ypix
+        self.fieldfree = fieldfree
+        self.depletion = depletion
+        self.considersplits = considersplits
+        self.steps = steps
         
         # Initialize the pixels array
         self.pixels = np.zeros((xpix,ypix))
@@ -222,6 +240,8 @@ class Detector(FlatComponent):
         
         Notes:
         - The original self.pixels is unmodified by this function
+        - This function will consider split events if self.considersplits is
+            True
         '''
         if rays is None:
             return self.pixels
@@ -233,26 +253,213 @@ class Detector(FlatComponent):
             self.pixels += np.random.poisson(lam=self.noisevalues,
                                             size=self.pixels.shape)
         
-        x,y = self.getPosns(rays)
+        if (self.considersplits):
+            self.pixels += self.split(rays,self.steps)
         
-        # Move axes so that the origin is in the bottom-left corner
-        x += self.l.value/2
-        y += self.w.value/2
+        else:
         
-        # Find out the length and width of each pixel
-        xperpix = self.l.value / self.xpix
-        yperpix = self.w.value / self.ypix
+            x,y = self.getPosns(rays)
+            
+            # Move axes so that the origin is in the bottom-left corner
+            x += self.l.value/2
+            y += self.w.value/2
+            
+            # Find out the length and width of each pixel
+            xperpix = self.l.value / self.xpix
+            yperpix = self.w.value / self.ypix
+            
+            xposns = (x // xperpix).astype(int)
+            yposns = (y // yperpix).astype(int)
+            
+            self.pixels[xposns,yposns] += (rays.wave*u.nm).to(u.eV,equivalencies=u.spectral()).value/3.65
         
-        xposns = (x // xperpix).astype(int)
-        yposns = (y // yperpix).astype(int)
+        return self.pixels
+    
+    
+    
+    def split(self, rays, steps = 50):
+        '''
+        Function split:
+        Calculates how the charge cloud produced by each photon spill out into
+            adjacent pixels. The algorithm works by considering small slices
+            of the charge cloud and figuring out when the slice moves into 
+            adjacent pixels. Using the area of the slice and a Gaussian integral,
+            each slice will deposit charge into nearby pixels.
         
-        temppixels = np.copy(self.pixels)
+        Inputs:
+        rays - The photons you want to simulate
+        steps - The number of slices you want to simulate. A smaller number is
+            faster but less accurate. Accuracy is most important for large 
+            charge clouds.
         
-        # I'm sure there's a faster way to do this:
-        for i in range(len(xposns)):
-            temppixels[xposns[i]][yposns[i]] += 1
+        Outputs:
+        arr - An array the same size as the Detector's pixel array, containing
+            the charge cloud binned into pixels. This array is added to the 
+            noise array by the view() function.
+        '''
+        # Get the x and y positions of the photons
+        xs = rays.x
+        ys = rays.y
         
-        return temppixels
+        # Offset the positions so the origin is in the corner
+        # This is also done for the noise matrix
+        xs += self.l.value/2
+        ys += self.w.value/2
+        
+        # Get the energies of the photons in eV
+        energies = (rays.wave*u.nm).to(u.eV,equivalencies=u.spectral()).value
+        
+        # Find the number of electrons created by dividing the energy by the
+        # quantum yield of Silicon
+        counts = energies / 3.65
+        
+        # Find the length and width of each pixel
+        pixlen = self.l.value / self.xpix
+        pixwid = self.w.value / self.ypix
+        
+        # Fine the attenuation length of silicon at this energy in um
+        # Found using a 2nd order polynomial approximation to the attenuation
+        # length as a function of energy
+        attenlens = 5.86902987e-06*energies**2 + -4.128074e-03*energies + 9.79687932e-01
+        
+        # Find how far the photons penetrate into the Silicon using the 
+        # attenuation length and the field free depth
+        d = (self.fieldfree.to(u.um) - (attenlens*u.um)).to(u.mm).value
+        
+        # Charge cloud is a Gaussian with FWHM=2d, find the standard deviation
+        # of the distribution
+        sigma = d / (np.sqrt(2*np.log(2)))
+        
+        # Initialize the list of slices we will be using in the future
+        thetas = np.linspace(0.0001,2*np.pi,steps)
+        
+        # Find the angle that once slice subtends
+        arc = np.mean(np.diff(thetas) / (2*np.pi))
+        
+        # Initialize a blank initial array
+        arr = np.zeros((self.xpix,self.ypix))
+    
+        for i in range(len(thetas)):
+            
+            theta = thetas[i]
+            costheta = np.cos(theta)
+            sintheta = np.sin(theta)
+            
+            # Find the coordinates of the pixel that each photon starts at
+            xpos = (xs/pixlen).astype(int)
+            ypos = (ys/pixwid).astype(int)
+            
+            # Find the offset of each photon's initial position from the center
+            # of the pixel
+            dx = pixlen/2 - (xs % pixlen)
+            dy = pixwid/2 - (ys % pixwid)
+            
+            # We need to keep track of how many vertical and horizontal steps
+            # that each photon has undergone so far
+            num_horiz_steps = np.zeros(len(xs))
+            num_vert_steps = np.zeros(len(xs))
+            
+            # This will keep track of the distance between the cloud's center 
+            # and the previous pixel boundary
+            prev_r = np.zeros(len(xs))
+            
+            # X and Y step are either 1 or -1 that depends on the direction of theta
+            ystep = int(costheta / np.abs(costheta))
+            xstep = int(sintheta / np.abs(sintheta))
+            
+            # This will keep track of the distance between the cloud's center 
+            # and the next pixel boundary
+            r = np.zeros(len(xs))
+            
+            # Keeps track of the photons which are done
+            done = np.zeros(len(xs)).astype(bool)
+            
+            while True:
+                
+                # calculate the two potential step sizes
+                next_horiz_step = (((2*num_horiz_steps + 1) * pixwid * ystep / 2) - dx) / np.abs(costheta)
+                next_vert_step = (((2*num_vert_steps + 1) * pixlen * xstep / 2) - dy) / np.abs(sintheta)
+                
+                # Find which photons are done
+                newdone = np.logical_or(np.logical_or(xpos < 0, xpos >= self.xpix),
+                                    np.logical_or(ypos < 0, ypos >= self.ypix))
+                done = np.logical_or(newdone,done)
+                
+                # Find the photons undergoing their last step
+                # ------------------------------------------
+                laststep = np.logical_and(np.abs(next_horiz_step) > d, np.abs(next_vert_step) > d)
+                
+                # Block the photons which are done
+                laststep = np.logical_and(laststep,np.logical_not(done))
+                
+                # Update the positions of the laststep photons
+                r[laststep] = d[laststep]
+                
+                # This is the indices of the photons undergoing their last step
+                ind = np.where(laststep)[0]
+                
+                # Iterate through the last indices to find how many counts need to 
+                # be added to this pixel
+                for i in ind:
+                    arr[xpos[i],ypos[i]] += quad(lambda x: weightedgauss(x), prev_r[i]/sigma[i], r[i]/sigma[i])[0] * energies[i] / steps
+                
+                # Remember that these photons do not need to be iterated anymore
+                done[laststep] = 1
+                
+                # Stop if all photons are done
+                if done.all():
+                    break
+                
+                # Find the photons which need to take a horizontal step
+                # --------------------------------------------------------
+                needhoriz = np.abs(next_horiz_step) <= np.abs(next_vert_step)
+                
+                # Block the photons which are done
+                needhoriz = np.logical_and(needhoriz,np.logical_not(done))
+                
+                # update the positions of the photons
+                r[needhoriz] = next_horiz_step[needhoriz]
+                
+                # This is the indices of the photons undergoing horiz steps
+                ind = np.where(needhoriz)[0]
+                
+                # Iterate through the horiz indices to find how many counts need to 
+                # be added to this pixel
+                for i in ind:
+                    arr[xpos[i],ypos[i]] += quad(lambda x: weightedgauss(x), prev_r[i]/sigma[i], r[i]/sigma[i])[0] * energies[i] / steps
+                
+                # Update ypositions
+                ypos[needhoriz] += ystep
+                num_horiz_steps[needhoriz] += 1
+                
+                # Find the photons which need to take a vertical step
+                # --------------------------------------------------------
+                needvert = np.abs(next_vert_step) < np.abs(next_horiz_step)
+                
+                # Block the photons which are done
+                needvert = np.logical_and(needvert,np.logical_not(done))
+                
+                # update the positions of the photons
+                r[needvert] = next_vert_step[needvert]
+                
+                # This is the indices of the photons undergoing horiz steps
+                ind = np.where(needvert)[0]
+                
+                # Iterate through the horiz indices to find how many counts need to 
+                # be added to this pixel
+                for i in ind:
+                    arr[xpos[i],ypos[i]] += quad(lambda x: weightedgauss(x), prev_r[i]/sigma[i], r[i]/sigma[i])[0] * energies[i] / steps
+                
+                # Update ypositions
+                xpos[needvert] += xstep
+                num_vert_steps[needvert] += 1
+                
+                prev_r = r.copy()
+        
+        # Normalize Gaussian
+        arr *= 5.013256549262
+        
+        return arr
 
     ## Trace Function:
     
@@ -276,3 +483,15 @@ class Detector(FlatComponent):
         '''
         self.trace_to_surf(rays)
         return self.removemissed(rays,considerweights,eliminate)
+
+@np.vectorize
+def weightedgauss(r):
+    '''
+    Weighted Gauss is a function that needs to be integrated by the split() 
+        function. If a Gaussian function is defined as G(r), the weighted
+        gaussian is W(r) = r*G(r).
+    '''
+    # Assumes mean of 0 and a standard deviation of zero
+    return r * np.exp(-.5 * r**2) / np.sqrt(2 * np.pi)
+
+
